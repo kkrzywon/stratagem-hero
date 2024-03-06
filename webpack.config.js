e198983a1c6065c93004fffa954b254c18cf04a1
kkrzywon <kkrzywon@gmail.com>
2024-03-06 15:27:46 -0500

    created initial package.json and webpack.config.js as well as gitignore, installed dev dependencies foir babel, scss/css loaders as well

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './index.html'),
+    }),
+  ],
+  devServer: {
+    static: {
+      publicPath: '/build',
+      directory: path.join(__dirname, 'build'),
+    },
+  }
+}
